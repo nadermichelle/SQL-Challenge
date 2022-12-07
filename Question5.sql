f3bec072f35a48d790d90853ccfec1350f986179
nadermichelle <83132206+nadermichelle@users.noreply.github.com>
2022-12-06 23:14:09 -0500

    Update Question5.sql

--- Question5.sql
+++ Question5.sql
@@ -1,4 +1,4 @@
-SELECT RIGHT(store_location, 2), sum(revenue)
+SELECT RIGHT(store_location, 2) AS store_location, sum(revenue) AS total_revenue
 FROM store_revenue 
 GROUP BY 1 
 ORDER BY sum(revenue) DESC LIMIT 10;

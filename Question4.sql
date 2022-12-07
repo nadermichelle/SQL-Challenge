3e400aab3a0ae80ab5c7ad8d2b8f27ada48a5d73
nadermichelle <83132206+nadermichelle@users.noreply.github.com>
2022-12-06 23:13:43 -0500

    Update Question4.sql

--- Question4.sql
+++ Question4.sql
@@ -1,3 +1,3 @@
-SELECT MD.geo, sum(MD.clicks) AS total_clicks, sum(MD.impressions) AS total_impressions, sum(SR.revenue) AS total_revenue, sum(SR.revenue)/sum(MD.clicks) AS revenue_clicks_ratio, sum(SR.revenue)/sum(MD.impressions) as revenue_impressions_ratio
+SELECT MD.geo, sum(MD.clicks) AS total_clicks, sum(MD.impressions) AS total_impressions, sum(SR.revenue) AS total_revenue, sum(SR.revenue)/sum(MD.clicks) AS revenue_clicks_ratio, sum(SR.revenue)/sum(MD.impressions) AS revenue_impressions_ratio
 FROM challenge.marketing_data MD INNER JOIN challenge.store_revenue SR ON MD.geo = RIGHT(SR.store_location, 2)
 GROUP BY 1;

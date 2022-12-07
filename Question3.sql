1893a56918e7d65557c061e6ed1c53379ef7a349
nadermichelle <83132206+nadermichelle@users.noreply.github.com>
2022-12-06 23:12:56 -0500

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,4 +1,4 @@
-SELECT MD.date, MD.geo, sum(MD.clicks), sum(MD.impressions), sum(SR.revenue)
+SELECT MD.date, MD.geo, sum(MD.clicks) as total_clicks, sum(MD.impressions) as total_impressions, sum(SR.revenue) as total_revenue
 FROM challenge.marketing_data MD INNER JOIN challenge.store_revenue SR
 ON MD.date = SR.date AND MD.geo = RIGHT(SR.store_location, 2)
 GROUP BY 1,2;
